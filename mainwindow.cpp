3dbe7d1332befc942d56357cfe1b49523b8bcd91
Latlante <corentin.sapiens@gmail.com>
2018-08-14 16:45:52 +0200

    Cleanup MainWindow

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,24 +3,19 @@
 
 #include <QMessageBox>
 #include "instancemanager.h"
-#include "tcpserverclients.h"
+
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
-    ui(new Ui::MainWindow),
-    m_serverClient(new TcpServerClients())
+    ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
     new InstanceManager();
 
-    if(InstanceManager::instance()->checkInstanceExeExists())
-        m_serverClient->start();
-    else
-        qCritical() << __PRETTY_FUNCTION__ << "Instance exe not found";
 
     ui->listView_Users->setModel(&m_modelUsers);
-    connect(m_serverClient, &TcpServerClients::newUserConnected, &m_modelUsers, &ModelListUsers::addNewUser);
-    connect(m_serverClient, &TcpServerClients::userDisconnected, &m_modelUsers, &ModelListUsers::removeAUser);
+    //connect(m_serverClient, &TcpServerClients::newUserConnected, &m_modelUsers, &ModelListUsers::addNewUser);
+    //connect(m_serverClient, &TcpServerClients::userDisconnected, &m_modelUsers, &ModelListUsers::removeAUser);
 
     ui->tableView_Games->setModel(&m_modelGames);
     connect(InstanceManager::instance(), &InstanceManager::newGameCreated, &m_modelGames, &ModelTableGames::addNewGame);
@@ -31,10 +26,6 @@ MainWindow::MainWindow(QWidget *parent) :
 MainWindow::~MainWindow()
 {
     InstanceManager::deleteInstance();
-
-    if(m_serverClient->isRunning())
-        m_serverClient->close();
-    delete m_serverClient;
 
     delete ui;
 }
